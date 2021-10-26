f5f74008354be3819e93b6effd005aac8b00d878
MD. Jahangir Alom Pial <cseselimreza@gmail.com>
2021-10-26 16:55:09 +0600

    model done

--- app/Models/Modules/QuestionBank/QuestionAttachment.php
+++ app/Models/Modules/QuestionBank/QuestionAttachment.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class QuestionAttachment extends Model
 {
 
+    protected $table = 'questions';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'question_id',
+        'file',
+    ];
+
+
+    /**
+     * Get the phone associated with the user.
+     */
+    public function questionAttachment()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
 }
